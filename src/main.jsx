f802531b112f3deb12ec839bdb31bd92fd97945b
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-11 05:22:48 +0600

    1st

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Main from './components/Layouts/Main';
+import Home from './components/Home/Home';
+import Order from './components/OrderReview/Order';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [{
+      path: '/',
+      element: <Home></Home>,
+      loader:()=>fetch('https://raw.githubusercontent.com/ProgrammingHero1/t-shirt-data/main/tshirt.json')
+    },
+      {
+        path: 'review',
+        element:<Order></Order>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
