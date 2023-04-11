83781f0c98d5676fdc01a4810fe23d31f82d16d7
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-11 06:30:05 +0600

    consitional rendaring

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Cart = ({cart,handleRemoveFromCart}) => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
+    let message;
+    if (cart.length === 0) {
+        message= <p>Please add some product</p>
+    }
     return (
         <div>
             <h2>cart summary:{cart.length}</h2>
+                {cart.length > 2 ? <span>Aro kino </span> : <span>fokira</span>}
+            {message}
             {
                 cart.map((item) => <p key={item._id}>{item.name} <button onClick={()=>handleRemoveFromCart(item._id)}>X</button> </p>)
             }
